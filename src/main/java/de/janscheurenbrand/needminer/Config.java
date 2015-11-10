af147e70eb2808a4aa2e96e2b845d09d28fb0143
Jan Scheurenbrand <mail@janscheurenbrand.de>
2015-11-10 08:54:06 +0100

    Use global constant to set directory root

---
+++ src/main/java/de/janscheurenbrand/needminer/Config.java
@@ -0,0 +1,8 @@
+package de.janscheurenbrand.needminer;
+
+/**
+ * Created by janscheurenbrand on 10/07/15.
+ */
+public class Config {
+    public static final String BASE_PATH = System.getProperty("user.home")+"/needminer/";
+}
